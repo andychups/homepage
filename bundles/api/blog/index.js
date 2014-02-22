3442bc52ef515ecec921ae05b9e9200f5a94935b
andychups <andychups@ya.ru>
2014-02-22 14:49:33 +0400

    Фикс код-стайла

--- bundles/api/blog/index.js
+++ bundles/api/blog/index.js
@@ -8,34 +8,46 @@ page.post = function (req, res) {
             author: req.body.author,
             message: req.body.message
         })
-        .then(function () {
-            res.send({'status': 'SUCCESS'});
-        }, function (err) {
-            res.send(500, {'status': 'FAILED', 'message': err});
-            console.error('Post not added: ', err);
-        });
+        .then(
+            function () {
+                res.send({'status': 'SUCCESS'});
+            },
+
+            function (err) {
+                res.send(500, {'status': 'FAILED', 'message': err});
+                console.error('Post not added: ', err);
+            }
+        );
 };
 
 page.list = function (req, res) {
     blog
         .list()
-        .then(function (threads) {
-            res.send({'status': 'SUCCESS', 'data': threads});
-        }, function (err) {
-            res.send(500, {'status': 'FAILED', 'message': err});
-            console.error('List with posts not showed: ', err);
-        });
+        .then(
+            function (threads) {
+                res.send({'status': 'SUCCESS', 'data': threads});
+            },
+
+            function (err) {
+                res.send(500, {'status': 'FAILED', 'message': err});
+                console.error('List with posts not showed: ', err);
+            }
+        );
 };
 
 page.show = function (req, res) {
     blog
         .show(req.params.title)
-        .then(function (threads) {
-            res.send({'status': 'SUCCESS', 'data': threads});
-        }, function (err) {
-            res.send(500, {'status': 'FAILED', 'message': err});
-            console.error('Cant show post: ', err);
-        });
+        .then(
+            function (threads) {
+                res.send({'status': 'SUCCESS', 'data': threads});
+            },
+
+            function (err) {
+                res.send(500, {'status': 'FAILED', 'message': err});
+                console.error('Cant show post: ', err);
+            }
+        );
 };
 
 module.exports = page;
